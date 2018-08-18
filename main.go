62a881cce2dbaadf3484cfd364177154349e4237
Drew Morris <dhmorris@gmail.com>
2018-08-18 19:57:05 -0400

    Cleaning up the script a bit

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"image"
 	"log"
 	"math"
@@ -11,50 +12,69 @@ import (
 	"github.com/disintegration/imaging"
 )
 
-func createsq(src image.Image, i int, j int, m int) {
+// create the squares by cropping and saving to the output dir
+func createsq(img image.Image, size int, i int, j int, m int) {
 	filepath := filepath.Join(".", "output", strconv.Itoa(i), strconv.Itoa(j), strconv.Itoa(m)+".png")
-	size := 256
-	total := int(math.Exp2(float64(i))) * size
-	src = imaging.Resize(src, total, 0, imaging.Lanczos)
+
+	// create the rect and crop
 	x0 := j * size
 	x1 := x0 + size
 	y0 := m * size
 	y1 := y0 + size
 	rect := image.Rect(x0, y0, x1, y1)
-	src = imaging.Crop(src, rect)
+	img = imaging.Crop(img, rect)
 
+	// save
 	log.Printf("Saving %s", filepath)
-	err := imaging.Save(src, filepath)
+	err := imaging.Save(img, filepath)
 	if err != nil {
 		log.Fatalf("failed to save image: %v", err)
 	}
 }
 
-func main() {
-	// Open a test image.
-	src, err := imaging.Open("map.png")
-	if err != nil {
-		log.Fatalf("failed to open map: %v", err)
-	}
-
+// loop through all the squares
+func loop(src image.Image, si int, sj int, sm int) {
 	// count from 0 to 7
-	for i := 0; i <= 7; i++ {
+	for i := si; i <= 7; i++ {
 		// calculate the number of layers
 		limit := int(math.Exp2(float64(i))) - 1
 
 		// count from 0 to limit
-		for j := 0; j <= limit; j++ {
+		for j := sj; j <= limit; j++ {
 			// create the dir
 			dirpath := filepath.Join(".", "output", strconv.Itoa(i), strconv.Itoa(j))
-			err = os.MkdirAll(dirpath, os.ModePerm)
+			err := os.MkdirAll(dirpath, os.ModePerm)
 			if err != nil {
 				log.Fatalf("failed to create directory: %v", err)
 			}
 
+			// resize at this scale
+			size := 256
+			scale := int(math.Exp2(float64(i))) * size
+			img := imaging.Resize(src, scale, 0, imaging.Lanczos)
+
 			// count the same limit for the images
-			for m := 0; m <= limit; m++ {
-				createsq(src, i, j, m)
+			for m := sm; m <= limit; m++ {
+				createsq(img, size, i, j, m)
 			}
 		}
 	}
+}
+
+// main
+func main() {
+	// set flags
+	mappath := flag.String("map", "map.png", "location of the map image")
+	si := flag.Int("i", 0, "starting i value")
+	sj := flag.Int("j", 0, "starting j value")
+	sm := flag.Int("m", 0, "starting m value")
+	flag.Parse()
+
+	// Open the map
+	src, err := imaging.Open(*mappath)
+	if err != nil {
+		log.Fatalf("failed to open map: %v", err)
+	}
+
+	loop(src, *si, *sj, *sm)
 }
